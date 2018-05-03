434476c7d2c097a5143003d0f2b5d4c156968795
Charlie Merrill <fcharliemerrill@gmail.com>
2018-05-03 14:28:18 -0700

    adds mobile vid animationand other small changes

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, HostListener, Output } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit, HostListener, Output } from '@angular/core';
 import { Util } from '../shared/util/util'
 
 declare var $: any;
@@ -10,7 +10,7 @@ declare var $: any;
   providers: [Util],
   host: {'(window:scroll)': 'track($event)'},
 })
-export class HomepageComponent implements OnInit, OnDestroy {
+export class HomepageComponent implements OnInit, OnDestroy, AfterViewInit {
 
   @Output() isMobileOrTablet:boolean = false;
   isSafari:boolean = false;
@@ -76,12 +76,8 @@ export class HomepageComponent implements OnInit, OnDestroy {
 
       if (/msie\s|trident\/|edge\//i.test(ua)) {
         this.isIE = true;
+        alert('We want our users to have the best experience possible. For optimal performance, we recommend using Google Chrome or Mozilla Firefox as your browser.');
       }
-
-
-    setTimeout(() => {
-      (<HTMLElement>document.querySelector('#top-design')).style.opacity = '1';
-    }, 1500);
 
     // Scoll implemenation
     // window.addEventListener('scroll', this.onScroll, true )
@@ -426,6 +422,12 @@ export class HomepageComponent implements OnInit, OnDestroy {
       });
 
     } // End ngOnInit
+
+    ngAfterViewInit() {
+      setTimeout(() => {
+      (<HTMLElement>document.querySelector('#top-design')).style.opacity = '1';
+    }, 1000);
+    }
 
 }
 
