a6253af53bd5cf1a48df9bdc81cdfa622eecbcc4
Christopher Kendrick <chris.kendricks07@gmail.com>
2018-07-13 14:46:51 -0700

    equal col with directive

--- src/app/shared/directives/equal-height-col.directive.ts
+++ src/app/shared/directives/equal-height-col.directive.ts
@@ -8,11 +8,32 @@ declare var $:any
 })
 export class EqualHeightColDirective {
 
-
+  colHeight:number[] = []
+  el:ElementRef
 
   constructor(el: ElementRef, app:AppComponent) {
-  	var cols = $(el.nativeElement[0]).children('.col')
-  	console.log(cols)
+  	this.el = el
+  	$(document).ready(()=>{
+  		this.resizeColumns()
+  	})
+  	$(window).resize(()=>{
+  		this.resizeColumns()
+  	})
+  }
+
+  resizeColumns() {
+	var cols = $(this.el.nativeElement).children()
+	cols.map($i => {
+		var col = $(cols[$i])
+		var height = col.outerHeight();
+		this.colHeight.push(height)	
+	})
+	var largestColHeight = Math.max.apply(null, this.colHeight)
+	console.log(largestColHeight)
+	cols.map($i => {
+		var col = $(cols[$i])
+		col.find('.white-bg').css({'min-height':largestColHeight})
+	})  	
   }
 
 }
