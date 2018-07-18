30de941a5b84ce59531d970424ec607e0c8f7cf0
Christopher Kendrick <chris.kendricks07@gmail.com>
2018-07-18 16:05:55 -0700

    form input component and settings page

---
+++ src/app/shared/directives/form-toggler-close.directive.spec.ts
@@ -0,0 +1,8 @@
+import { FormTogglerCloseDirective } from './form-toggler-close.directive';
+
+describe('FormTogglerCloseDirective', () => {
+  it('should create an instance', () => {
+    const directive = new FormTogglerCloseDirective();
+    expect(directive).toBeTruthy();
+  });
+});
