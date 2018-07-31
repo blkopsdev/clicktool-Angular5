9f335d1869df8e1e1a96807ff145a6cbe2466681
JurickJoling <jurick.joling@gmail.com>
2018-07-31 17:53:47 -0400

    modified:   src/app/shared/common/signup-modal/signup-modal.component.html 	modified:   src/app/shared/common/signup-modal/signup-modal.component.ts

--- src/app/shared/common/signup-modal/signup-modal.component.ts
+++ src/app/shared/common/signup-modal/signup-modal.component.ts
@@ -15,19 +15,38 @@ import { toUnicode } from 'punycode';
 export class SignupModalComponent implements OnInit {
   
   form: FormGroup;
+  isSubmitted: boolean = false;
+  result: any = null;
   
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.form = this.formBuilder.group({
-      email: [null, Validators.required],
-      name: [null, Validators.required]
-    })
+      name: ["", [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
+      email: ["", [Validators.required]],
+    });
   }
 
+  get name() { return this.form.get('name'); }
+  get email() { return this.form.get('email'); }
+
   onSubmit() {
-    // TODO: Use EventEmitter with form value
-    console.warn(this.form.value);
+    this.isSubmitted = true;
+    if (!this.form.valid)
+      return;
+    // Code to save the data
+    // userService.Save(this.register.value);
+    this.result = this.form.value;
+    console.log(this.result);
+    /* setTimeout(() => {
+      this.result = null;
+      this.reset();
+    }, 2000); */
+  }
+
+  reset() {
+    this.isSubmitted = false;
+    this.form.reset();
   }
 
 }
